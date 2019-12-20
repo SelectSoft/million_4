430f61678b667f15b10dd38a2ed48dd95ec84004
SelectSoft <52114175+SelectSoft@users.noreply.github.com>
2019-12-20 17:51:02 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,7 +38,7 @@ def item_scrapping(name):
 	while item_no <= total-1:
 		try:
 			try:
-				print("Scrapping item "+str(url_number)+" of "+str(total))
+				
 				
 				response = requests.get(url=url[item_no],timeout= 10)		
 				soup = BeautifulSoup(response.text, 'html.parser')
@@ -98,24 +98,24 @@ def item_scrapping(name):
 				item_no += 1
 				url_number += 1
 			except requests.ConnectionError:
-				print("No internet connection")
+				
 				while True:
 					try:
 						requests.get('https://www.google.com/')
-						print("Internet Connection available")		
+							
 						break
 					except requests.ConnectionError:
 						continue
 
 		except:
-			print("Error in Item: "+ str(url_number))
+			
 			item_no += 1
 			url_number += 1
 
 item_scrapping("files3.csv")
-data = pd.read_csv('Scrapped_Data3e.csv')
+data = pd.read_csv('Scrapped_Data_files3.csv.csv')
 conn = sqlite3.connect("data.sqlite")
 
-conn.execute("CREATE TABLE if not exists data ('Title','Prodcut_VPN','Brand','Price','Description','Images','Category','Sub_Category','Related_Products_VPN','URL')")
+conn.execute("CREATE TABLE if not exists data ('Title','Prodcut_VPN','Brand','Price','Description','Images','Category','Sub_Category','Related_Products_VPN','URL','UPC','Warranty','Packaging_info','Specificaiton')")
 
 data.to_sql("data", conn, if_exists='replace', index=False)
